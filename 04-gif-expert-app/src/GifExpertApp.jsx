b2c74f86fa0a5361237d564b4a6cbea04e057a61
JuanEsVal <juanes.valenciag@gmail.com>
2022-11-03 10:01:14 -0500

    Complete 04-gif-expert-app whith custom hook useFetchGifs.js & barrel files

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,12 +1,37 @@
-//Despues de la version 17 de React ya no necesitamos importar React from 'react
-// import React from 'react'
-
-
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
 
 export const GifExpertApp = () => {
-  return (
-    <>
-      <h1>GitExpertApp</h1>
-    </>
-  )
+    
+    const [ categories, setCategories ] = useState([ 'One Punch' ]);
+    
+    const onAddCategory = ( newCategory ) => {
+        if ( categories.includes(newCategory) ) return;
+        setCategories([ newCategory, ...categories ]);
+    }
+    
+
+    return (
+        <>
+
+            <h1>GifExpertApp</h1>
+
+    
+            <AddCategory 
+                onNewCategory={ (value) => onAddCategory(value) }
+            />
+
+            { 
+                categories.map( ( category ) => (
+                    <GifGrid 
+                        key={ category } 
+                        category={ category } />
+                ))
+            }
+
+
+
+
+        </>
+    )
 }
